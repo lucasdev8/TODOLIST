3e741f9511f3b4496cbc9487f4d4842316a592a8
lucasdev8 <lucassilv321234@gmail.com>
2023-10-13 19:04:11 -0300

    upload application

---
+++ src/main/java/br/com/lucasdev/todolist/users/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.lucasdev.todolist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
